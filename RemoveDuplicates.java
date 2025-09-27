b926aeca7d9c6c6ce90776aeb6c5dc56a4b26f47
SM267 <sm30124872@gmail.com>
2025-09-27 10:53:58 +0530

    Updation

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,6 +1,19 @@
 import java.util.*;
-
-public class RemoveDuplicates {
+public class RemoveDuplicates 
+{
+    
+    public static void Sort(int [] nums){
+        int n=nums.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+    }
     public static int check(int[] nums) {
         int ii=0;
         for(int i=0;i<nums.length;i++){
@@ -18,6 +31,12 @@ public class RemoveDuplicates {
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
+        RemoveDuplicates.Sort(nums);
+        System.out.println("Array after sorting:");
+        for(int i=0;i<n;i++){
+            System.out.print(nums[i]+" ");
+        }
+        System.out.println();
         int k=RemoveDuplicates.check(nums);
         System.out.println("Number of Unique Numbers:" +k);
         System.out.println("Array after removing duplicates:");
